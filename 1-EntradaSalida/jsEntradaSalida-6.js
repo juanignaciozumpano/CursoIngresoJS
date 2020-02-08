8b444a5fdda2e0fe2b844eae871942a44a18fefd
juanignaciozumpano <60760383+juanignaciozumpano@users.noreply.github.com>
2020-02-07 21:57:04 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,18 +3,21 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-    var num1
-    var num2
+    var num1;
+    var num2;
+    var resultado;
 
     num1 = document.getElementById("numeroUno").value;
     num2 = document.getElementById("numeroDos").value;
 
-    parseInt(num1);
-    parseInt(num2);
-    sumar = (num1 + num2);
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    resultado = (num1 + num2);
 
 
-    alert("el resultado es " + sumar);
 
-}falta completar terminar
+    alert("el resultado es " + resultado);
+
+}
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,29 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var importe;
+    var resultado;
 
-	
+    importe = document.getElementById("sueldo").value;
+    importe = parseInt(importe);
+
+    resultado = (importe + importe * 10 / 100)
+
+    document.getElementById("resultado").value = resultado;
+    
+    
+
+   /* var sueldo;
+    var aumento;
+    var sueldoConAumento;
+
+    sueldo = parseFloat(sueldo);
+    sueldo = document.getElementById("sueldo").value;
+    aumento = sueldo * 10 / 100;
+    sueldoConAumento = sueldo + aumento;
+
+    document.getElementById("resultado").value = sueldoConAumento;
+*/
 }
+
+
