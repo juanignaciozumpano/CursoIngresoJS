9f9066d53bf6a785c78d06f7a9b26d4d00ca1cd6
juanignaciozumpano <60760383+juanignaciozumpano@users.noreply.github.com>
2020-02-14 19:24:53 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,21 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numero;
+	var minimo = 1;
+	var maximo = 10;
+
+	numero = Math.floor(Math.random() * ((maximo + 1) - minimo) + minimo);
+
+	if(numero == 9 || numero == 10){
+		alert("Excelente " + "tu nota es: " + numero);
+	}
+	else if(numero >= 4){
+		alert("APROBO " + "su nota es: " + numero);
+	}
+	else{
+		alert("Vamos la proxima se puede. Su nota es: " + numero);
+	}
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,6 +2,12 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numero;
+	var minimo = 1;
+	var maximo = 10;
 
+	numero = Math.floor(Math.random() * ((maximo + 1) - minimo) + minimo);
+
+	alert(numero);
 
 }//FIN DE LA FUNCIÓN
