f4fd52c11e7af76d5f1c5223cbbd46938968c9e3
juanignaciozumpano <60760383+juanignaciozumpano@users.noreply.github.com>
2020-02-14 22:19:48 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	alert('iteración while');
+	var i = 1;
 
+	while( i <= 10){
+		console.log(i);
+
+	i++;
+
+	}
+
+/* for ( var j = 1; j <= 10; j++){
+	console.log(J); */
+}
 
 }//FIN DE LA FUNCIÓN
